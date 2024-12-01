68cf0388928896cc1d8dab403d8bd34929a57af6
Prithvi Anil Kumar <prithvi.anilkumar@cred.club>
2024-12-02 00:03:29 +0530

    Init commit
    - Problem 1

---
+++ .cpcache/5F22BC824438EA028C1757BCFD187D52.cp
@@ -0,0 +1,1 @@
+src:/Users/prithvianilkumar/.m2/repository/org/clojure/clojure/1.12.0/clojure-1.12.0.jar:/Users/prithvianilkumar/.m2/repository/org/clojure/core.specs.alpha/0.4.74/core.specs.alpha-0.4.74.jar:/Users/prithvianilkumar/.m2/repository/org/clojure/spec.alpha/0.5.238/spec.alpha-0.5.238.jar
---
+++ src/one.clj
@@ -0,0 +1,37 @@
+(ns one
+  (:require [clojure.string :as str]))
+
+(defn unzip [a b]
+      (vector
+        (conj (nth a 0) (Integer/parseInt (nth b 0)))
+        (conj (nth a 1) (Integer/parseInt (nth b 1)))))
+
+(defn read-input
+      [input-path]
+      (reduce unzip [[] []] (map #(str/split % #"   ") (str/split-lines (slurp input-path)))))
+
+(defn distance-between-lists
+      [x y]
+      (reduce + (map (comp #(abs (- (nth % 0) (nth % 1))) vector)
+                     (sort x) (sort y))))
+
+(defn similarity-score
+      [x y]
+      (let [y-frequencies (frequencies y)]
+        (reduce + (map #(* % (or (y-frequencies %) 0)) x))
+        ))
+
+(defn solve-first [input-path]
+      (let
+        [[x y] (read-input input-path)]
+        (println (distance-between-lists x y))))
+
+(defn solve-second [input-path]
+      (let
+        [[x y] (read-input input-path)]
+        (println (similarity-score x y))))
+
+(solve-first "./problem-input/one/example.txt")
+(solve-first "./problem-input/one/problem.txt")
+(solve-second "./problem-input/one/example.txt")
+(solve-second "./problem-input/one/problem.txt")
