8e79891ca2f128f466bcc283658cfb09a985f6d3
prithvianilk <thepirateking3d2y@gmail.com>
2024-12-06 01:00:01 +0530

    Day 5 part 1 done

---
+++ src/five.clj
@@ -0,0 +1,48 @@
+(ns five
+  (:require [clojure.string :as str]
+            [input-reader]))
+
+(defn get-order [line] (map ^[String] Integer/parseInt (str/split line #"\|")))
+
+(defn get-rules
+      ([lines]
+       (get-rules lines {}))
+
+      ([lines rules]
+       (if (= (first lines) "")
+         {:rules rules :remaining-lines lines}
+         (let [[left right] (get-order (first lines))]
+           (get-rules (rest lines) (assoc rules left (conj (if (contains? rules left) (rules left) #{}) right))))
+         ))
+      )
+
+(defn get-pages [lines]
+      (->> lines
+           (map #(str/split % #","))
+           (map #(map ^[String] Integer/parseInt %))))
+
+(defn get-rules-and-pages [input-path]
+      (let [{rules :rules remaining-lines :remaining-lines}
+            (get-rules (input-reader/read-lines input-path))]
+        {:ordering-rules   rules
+         :pages-to-produce (get-pages (rest remaining-lines))}))
+
+(defn correctly-ordered?
+      ([page ordering-rules] (correctly-ordered? (rest page) (first page) ordering-rules))
+
+      ([page prev ordering-rules]
+       (cond
+         (empty? page) true
+         (contains? (ordering-rules prev) (first page)) (correctly-ordered? (rest page) (first page) ordering-rules)
+         :else false))
+      )
+
+(defn seq-middle [coll] (nth coll (/ (count coll) 2)))
+
+(defn solve-one [input-path]
+      (let [{ordering-rules :ordering-rules pages-to-produce :pages-to-produce}
+            (get-rules-and-pages input-path)]
+        (->> pages-to-produce
+             (filter #(correctly-ordered? % ordering-rules))
+             (map seq-middle)
+             (reduce +))))
